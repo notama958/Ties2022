bc498313a765dfe7d0db5111a2e1955ebb513e4e
Yen Tran <yen.o.tran@tuni.fi>
2021-11-26 17:56:37 +0200

    opencv => example

---
+++ example/app_v2.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response
+
+import threading
+from camera_opencv_v2 import Camera
+
+
+app = Flask(__name__)
+
+camera = Camera()
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+def gen(camera):
+    """Video streaming generator function."""
+    print("hello worlsadkadnkas")
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+class webapp:
+    def __init__(self):
+        self.camera = camera
+
+    def modeselect(self, modeInput):
+        Camera.modeSelect = modeInput
+
+    def colorFindSet(self, H, S, V):
+        camera.colorFindSet(H, S, V)
+
+    def thread(self):
+        app.run(host='0.0.0.0', threaded=True)
+
+    def startthread(self):
+        # Define a thread for FPV and OpenCV
+        fps_threading = threading.Thread(target=self.thread)
+        # 'True' means it is a front thread,it would close when the mainloop() closes
+        fps_threading.setDaemon(False)
+        fps_threading.start()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
